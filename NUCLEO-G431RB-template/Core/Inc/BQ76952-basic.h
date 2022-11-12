9d89c022d1a7a8b4ae077afcc5584904249d16dc
xzips <aspenxe@gmail.com>
2022-11-12 12:15:38 -0500

    Add TestConnection function
    TestConnection() allows a simple interface to test if I2C communication (TX and RX) with the device is functioning.

---
+++ NUCLEO-G431RB-template/Core/Inc/BQ76952-basic.h
@@ -0,0 +1,48 @@
+/*
+ * BQ76952-basic.c
+ *
+ *  Created on: Nov. 12, 2022
+ *      Author: Aspen Erlandsson, IG: @xzips
+ *
+ * TI-BQ76952 Basic driver to cover the core functionalities necessary for future development,
+ * namely: Initiate a connection with the device, receive basic information such as device ID,
+ * as well as unit test cases as they apply to the BMS Core Functionality Driver.
+ *
+ */
+
+#define BQ76952
+
+
+
+static const uint8_t BQ76952_I2C_ADDRESS = 0b0011000 << 1;
+I2C_HandleTypeDef *PTR_BQ76952_I2C_HANDLE = &hi2c1;//placeholder
+
+
+/*
+ * Tests that I2C communication to the device is functional, the status variable is where
+ * the result will be stored. This buffer should be of length >= 24
+ */
+
+void TestConnection(char *status);
+
+
+
+#ifndef BQ76952_BASIC_H_
+#define BQ76952_BASIC_H_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* BQ76952_BASIC_H_ */
---
+++ NUCLEO-G431RB-template/Core/Src/BQ76952-basic.c
@@ -0,0 +1,48 @@
+/*
+ * BQ76952-basic.c
+ *
+ *  Created on: Nov. 12, 2022
+ *      Author: Aspen Erlandsson, IG: @xzips
+ *
+ * TI-BQ76952 Basic driver to cover the core functionalities necessary for future development,
+ * namely: Initiate a connection with the device, receive basic information such as device ID,
+ * as well as unit test cases as they apply to the BMS Core Functionality Driver.
+ *
+ */
+
+
+/*
+ * Tests that I2C communication to the device is functional, the status variable is where
+ * the result will be stored. This buffer should be of length >= 24
+ */
+void TestConnection(char* status)
+{
+	HAL_StatusTypeDef i2c_com_status;
+
+
+	//0x03 -> access safety status register A
+	i2c_com_status = HAL_I2C_Master_Transmit(PTR_BQ76952_I2C_HANDLE,
+											BQ76952_I2C_ADDRESS,
+											0x03, 1, 1000);
+
+	if (ret != HAL_OK)
+	{
+		strcpy((char*)buf, "BQ76952 I2C TX Error\r\n\0");
+		return;
+	}
+
+	i2c_com_status = HAL_I2C_Master_Receive(PTR_BQ76952_I2C_HANDLE,
+											BQ76952_I2C_ADDRESS,
+											status, 1, 1000);
+
+	if (ret != HAL_OK)
+	{
+		strcpy((char*)buf, "BQ76952 I2C RX Error\r\n\0");
+		return;
+	}
+
+	strcpy((char*)buf, "BQ76952 I2C Comms OK\r\n\0");
+}
+
+
+
