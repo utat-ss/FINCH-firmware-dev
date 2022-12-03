1514f680fb68c5394f84a3db14d7ddbc19184902
RichardLi8 <richard10m5n@outlook.com>
2022-12-02 22:40:49 -0500

    Added Basic Status Checks

---
+++ NUCLEO-G431RB-template/Core/Inc/BQ76952-status-check.h
@@ -0,0 +1,41 @@
+/*
+ * BQ76952-status-check.h
+ *
+ *  Created on: Nov. 18, 2022
+ *      Author: Richard Li
+ */
+
+
+
+#ifndef INC_BQ76952_STATUS_CHECK_H_
+#define INC_BQ76952_STATUS_CHECK_H_
+
+#include <stdint.h>
+
+const uint8_t STATUS_ADDRESS = 0x12; //status check address
+
+
+typedef enum
+{
+  CFGUPDATE    = 1,       //CONFIG_UPDATE
+  PCHG_MODE    = 1<<1,    //PRECHARGE MODE
+  SLEEP_EN     = 1<<2,    //SLEEP MODE ALLOWED
+  POR          = 1<<3,	  //RAM CONFIGURATIONS LOST FORM CONFIG_UPDATE EXIT
+  WD           = 1<<4,    //WATCHDOG TIMER RESET
+  COW_CHK      = 1<<5,    //OPEN WIRE CHECKS
+  OTPW         = 1<<6,    //PENDING DATA
+  OTPB         = 1<<7,    //OTP WRITING PERMISSION
+  SEC          = 11<<8,   //SECURITY STATE
+  FUSE 		   = 1<<10,   //FUSE
+  SS		   = 1<<11,   //SAFETY FAULT
+  PF		   = 1<<12,   //PERMANENT FAULT
+  SDM		   = 1<<13,   //SHUTDOWN MODE
+  SLEEP 	   = 1<<15    //SLEEP MODE
+} Status_TypeDef;
+
+
+
+
+void requestStatus(uint16_t buff [], uint16_t position);
+
+#endif /* INC_BQ76952_STATUS_CHECK_H_ */
---
+++ NUCLEO-G431RB-template/Core/Src/BQ76952-status-check.c
@@ -0,0 +1,157 @@
+#include "BQ76952-basic.h"
+#include "BQ76952-status-check.h"
+#include <string.h>
+#include <stdio.h>
+
+/*
+ * Pass in the position integer, see ENUM in Header File
+ * Returns the current status in the provided buffer
+ */
+void requestStatus(uint16_t buff [], uint16_t position){
+	HAL_StatusTypeDef ret;
+	buff [0] = STATUS_ADDRESS;
+	ret = HAL_I2C_Master_Transmit(PTR_BQ76952_I2C_HANDLE, BQ76952_I2C_ADDRESS, buff, 1, 1000);
+
+	if(ret != HAL_OK){
+		strcpy((char*)buff, "BQ76952 I2C TX Error\r\n");
+		return;
+	}
+	ret = HAL_I2C_Master_Receive(PTR_BQ76952, BQ76952_I2C_ADDRESS, buff, 1, 1000);
+	if(ret != HAL_OK){
+		strcpy((char*)buff, "BQ76952 I2C RX Error\r\n");
+		return;
+	}
+	uint16_t status = buff [0];
+	switch(position){
+		case CFGUPDATE:
+			if(position & status){
+				strcpy((char*)buff, "Config Update Is Enabled\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Config Update Is Disabled\r\n");
+			}
+			break;
+		case PCHG_MODE:
+			if(position & status){
+				strcpy((char*)buff, "Precharge Mode Enabled\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Precharge Mode Disabled\r\n");
+			}
+			break;
+		case SLEEP_EN:
+			if(position & status){
+				strcpy((char*)buff, "Sleep Mode Allowed\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Sleep Mode is Disabled\r\n");
+			}
+			break;
+		case POR:
+			if(position & status){
+				strcpy((char*)buff, "Full Reset has Occurred\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Full Reset has Not Occurred\r\n");
+			}
+			break;
+		case WD:
+			if(position & status){
+				strcpy((char*)buff, "Reset From Watchdog Timer Reset\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Reset Normally\r\n");
+			}
+			break;
+		case COW_CHK:
+			if(position & status){
+				strcpy((char*)buff, "Open Wire Check is Occurring\r\n");
+			}
+			else{
+				strcpy((char*)buff, "No Open Wire Check\r\n");
+			}
+			break;
+		case OTPW:
+			if(position & status){
+				strcpy((char*)buff, "OTP Writes are Pending\r\n");
+			}
+			else{
+				strcpy((char*)buff, "No Writes Pending to OTP \r\n");
+			}
+			break;
+		case OTPB:
+			if(position & status){
+				strcpy((char*)buff, "OTP Writes are Blocked\r\n");
+			}
+			else{
+				strcpy((char*)buff, "OTP Writes are Allowed\r\n");
+			}
+			break;
+		case SEC:
+			uint16_t val = ((position | (position<<1)) & status)>>8;
+			if(val == 0){
+				strcpy((char*)buff, "Device Not Initialized\r\n");
+			}
+			else if(val == 1){
+				strcpy((char*)buff, "Device is in FULLACCESSS Mode\r\n");
+			}
+			else if(val == 2){
+				strcpy((char*)buff, "Device is in UNSEALED Mode\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Device is in SEALED Mode\r\n");
+			}
+			break;
+		case FUSE:
+			if(position & status){
+				strcpy((char*)buff, "Fuse Asserted\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Fuse not Asserted\r\n");
+			}
+			break;
+		case SS:
+			if(position & status){
+				strcpy((char*)buff, "Safety Fault Triggered\r\n");
+			}
+			else{
+				strcpy((char*)buff, "No Safety Fault Triggered\r\n");
+			}
+			break;
+		case PF:
+			if(position & status){
+				strcpy((char*)buff, "Permanent Fault Triggered\r\n");
+			}
+			else{
+				strcpy((char*)buff, "No Permanent Fault Triggered\r\n");
+			}
+			break;
+		case SDM:
+			if(position & status){
+				strcpy((char*)buff, "Shutdown Occurred/Is Pending\r\n");
+			}
+			else{
+				strcpy((char*)buff, "No Shutdown Occurred\r\n");
+			}
+			break;
+		case SLEEP:
+			if(position & status){
+				strcpy((char*)buff, "Device Is in Sleep Mode\r\n");
+			}
+			else{
+				strcpy((char*)buff, "Device Is not in Sleep Mode\r\n");
+			}
+			break;
+		default:
+			strcpy((char*)buff, "Status Does Not Exist\r\n");
+	}
+}
+
+/**
+ * Not Completed
+ */
+uint16_t getVoltage(){
+	float voltage = 0;
+
+	return voltage;
+}
